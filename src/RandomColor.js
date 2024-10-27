d9f8774b0a50772060d5246e1e72063b5d238590
Patricia Johansson <pj222uc@student.lnu.se>
2024-10-27 19:52:58 +0100

    Add the randomize method, pass RandomColor test suite

--- src/RandomColor.js
+++ src/RandomColor.js
@@ -5,6 +5,7 @@
  */
 
 import Color from './Color.js'
+import { RGB } from './RGB.js'
 
 /**
  * Represents a random color.
@@ -16,5 +17,29 @@ export default class RandomColor extends Color {
   constructor () {
     super(0, 0, 0)
     this.randomize()
+  }
+
+  /**
+   * Randomizes the color.
+   */
+  randomize () {
+    const red = this.#getRandomRGBvalue()
+    const green = this.#getRandomRGBvalue()
+    const blue = this.#getRandomRGBvalue()
+    this.changeColor(new RGB(red, green, blue))
+  }
+
+  /**
+   * Get a random value [0, 255].
+   *
+   * @returns {number} A number between 0 and 255.
+   */
+  #getRandomRGBvalue () {
+    /**
+     * See @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+     */
+    const min = 0
+    const max = 255
+    return Math.floor(Math.random() * (max - min + 1) + min)
   }
 }
